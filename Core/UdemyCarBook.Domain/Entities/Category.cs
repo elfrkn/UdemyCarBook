61e209f051c15ebac15f8fe7f421c560fbe88662
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-07-26 15:26:41 +0200

    Add project files.

---
+++ Core/UdemyCarBook.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+       
+
+    }
+}
